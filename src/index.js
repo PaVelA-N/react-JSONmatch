8a163976b1d97cbceac51c8361e88f9e0b3bdbd3
PavelPC <pan_12001@yahoo.com>
2022-12-25 15:35:40 +0300

    добавлена загрузка локального JSON

--- src/index.js
+++ src/index.js
@@ -4,46 +4,57 @@ import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-const obj1 = {
-    akey: "a1f",
-    a1key: {a:"a1f"},
-    bkey: true,
-    c1key: {
-           a: { x: 1, y: 3 },
-           b: null,
-           c: [ "8", 7, [ 9, 0 ] ],
-           d: [ "8", 7, { x1: 2, y1: 5 }, 'a'],
-           e: [ "8", null, [ 9, 0 ] ],
-         },
-    dkey: ["d11", "d111", 112,23456, true, null, undefined],
-    d11key: ["d11", 112, [12,13]],
-    d12key: ["d11", 112, {x: 12, z: 13}],
-    ekey: {vv: 111, g: 1111},
-    fkey: {a: 1, ErrDontShowIt: {v: 1, Show2TimesThisLast: 12}},
-    gkey: null,
-     hkey: [121,122,{vm: 1, gm: 12},124],
-     jkey: [null],
-     kkey: undefined,
-     lkey: {z: null, g: undefined},
-};
+let obj1 = require('./JSON for match/data1.json');
+let obj2 = require('./JSON for match/data2.json');
+console.log('the json obj1: ', obj1);
+console.log('the json obj2: ', obj2);
 
-const obj2 = {
-    akey: "a1f",
-    a1key: "234c1",
-    b21key: true,
-    b22key: {vb: 21, g12b: 22},
-    c2key: "234c1",
-    dkey: ["d2","d23"],
-    ekey: {ve: 221, ge: 2222},
-    f21key: {a: 21, b: 2},
-    f22key2: {a: 21, b: {v: 221, g: 2}},
-    f23key3: {a: 21, b: {v: 221, g: {gv: 212, gg: 22}}},
-    gkey: null,
-    h21key: [21,22,23,24],
-    h22key: [21,22,{vm: 1, gm: 2},24],
-    jkey: null,
-    kkey: undefined,
-};
+// function loadFilefunction(){
+//     fetch('https://github.com/PaVelA-N/react-JSONmatch/blob/master/data1.json')
+//     .then(alert('test'))
+//     .then(console.log('test'))
+//     .then((response) => response.json())
+//     .then(console.log('response'))
+//     .then((json) => console.log(json));
+
+// let urliOfJSON = 'https://github.com/PaVelA-N/react-JSONmatch/blob/master/data1.json'
+//     let response = await fetch(urliOfJSON);
+
+//     if (response.ok) { // если HTTP-статус в диапазоне 200-299
+//       // получаем тело ответа (см. про этот метод ниже)
+//       let json = await response.json();
+//     } else {
+//       alert("Ошибка HTTP: " + response.status);
+//     }
+
+//     function readJson () {
+//         // http://localhost:8080
+//         // fetch('/Reading/api/file')
+//         fetch('https://github.com/PaVelA-N/react-JSONmatch/blob/master/data1.json')
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error("HTTP error " + response.status);
+//             }
+//             return response.json();
+//         })
+//         .then(json => {
+//             this.users = json;
+//             console.log(this);
+//         })
+//         .catch(function () {
+//             this.dataError = true;
+//         })
+//      }
+
+// let x = readJson();
+
+    // if (response.ok) { // если HTTP-статус в диапазоне 200-299
+    //     // получаем тело ответа (см. про этот метод ниже)
+    //     let json = await response.json();
+    //   } else {
+    //     alert("Ошибка HTTP: " + response.status);
+    //   }
+// }
 
 let compProps={};
 compProps.obj1 = obj1;
@@ -86,7 +97,7 @@ allUnicKeys(compProps);
                     let elem1 = showElement(obj1[item[0]]);
                     let elem2 = showElement(obj2[item[0]]);
                     if (elem1===elem2) {
-                        console.log('elem1 = elem2');
+                        // console.log('elem1 = elem2');
                         return <tr key={item}>
                         <td>{item[0]}</td>
                         <td style={{ color: 'green' }}> {'Общий ключ'}</td> 
@@ -94,7 +105,7 @@ allUnicKeys(compProps);
                         <td style={{ color: 'darkgreen', 'backgroundColor': 'lightgreen'}}>{elem2}</td>
                         </tr>;                
                     } else {
-                        console.log('elem1 < > elem2');
+                        // console.log('elem1 < > elem2');
                         return <tr key={item}>
                         <td>{item[0]}</td>
                         <td style={{ color: 'green'}}> {'Общий ключ'}</td> 
