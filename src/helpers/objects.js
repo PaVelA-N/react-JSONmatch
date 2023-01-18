fbeb643b61b6bc7c71087759fc5ab08a517fa41f
anseal <bottomless.box@gmail.com>
2023-01-18 21:50:41 +0300

    упростил uniqKeysFromObjects

--- src/helpers/objects.js
+++ src/helpers/objects.js
@@ -1,13 +1,9 @@
 export function uniqKeysFromObjects(obj1, obj2) {
-  let arr1 = Object.keys(obj1).sort();
-  let arr2 = Object.keys(obj2).sort();
-  let arrSame = arr1.filter((num) => arr2.includes(num));
-  let arr1Unic = arr1.filter((num) => !arr2.includes(num));
+  let arr1 = Object.keys(obj1);
+  let arr2 = Object.keys(obj2);
   let arr2Unic = arr2.filter((num) => !arr1.includes(num));
 
-  let allKeysArray = [...arrSame, ...arr1Unic, ...arr2Unic];
-
-  return allKeysArray;
+  return [...arr1, ...arr2Unic];
 }
 
 export function DefindMyTypeOf(obj) {
