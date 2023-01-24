a76f41da9b7b46f0e55729957e8cf44e1323d6f7
PavelPC <pan_12001@yahoo.com>
2023-01-24 20:21:47 +0300

    асинхронная фильтрация объекта тестовые последовательные промисы

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./index.css";
 
 import { uniqKeysFromObjects, DefindMyTypeOf } from './helpers/objects'
 
-let obj1 = require("./JSON for match/data1.json");
-let obj2 = require("./JSON for match/data2.json");
+let obj1 = require("./JSON for match/data11.json");
+let obj2 = require("./JSON for match/data12.json");
 
 let diff_Object1 = {KeyMark:{}, ValueMark:{}};
 let diff_Object2 = {KeyMark:{}, ValueMark:{}};
@@ -157,6 +157,8 @@ function MainFunction2(obj1, obj2, diffObj1, diffObj2) {
   });
   return resultArr;
 }
+
+
 
 function Make2DiffObjects(obj1, obj2, diff_Object1, diff_Object2){
   // diff_Object1.ValueMark;
@@ -349,11 +351,18 @@ function CreateMarkBelow2(obj, objName, diffObject, SpanMark){
   //  SpanMark = 'diffType' или "obj(i)UnicKey"
   // в objName - метка "-obj1" / "-obj2"
   let type = DefindMyTypeOf(obj);
+  // console.log(type);
   switch (type) {
-    case "primitiveType":    
+    // case ("primitiveType" || "undefinedType"):   
+    //   console.log("primitiveType || undefinedType"); 
+    //   diffObject = SpanMark + objName;
+    //   break;
+    case "primitiveType":   
+      // console.log("primitiveType"); 
       diffObject = SpanMark + objName;
       break;
     case "undefinedType":
+      // console.log("undefinedType"); 
       diffObject = SpanMark + objName;
       break;
     case "arrayType":
@@ -521,6 +530,10 @@ function getTableRow(cb, item, type, DiffObject, index) {
   } else {
     dataContent = cb(item);
   }
+
+  // dataContent = type === ('undefinedType' ||  'primitiveType') ?  
+  //   getSpan(cb, item, DiffObject, index) 
+  //   : cb(item);
    
 
   return (
@@ -789,15 +802,212 @@ function createTable3(obj1, obj2,diffObj1,diffObj2) {
   );
 }
 
-
-
 let transfArray;
 transfArray= Make2DiffObjects(obj1, obj2, diff_Object1, diff_Object2)
 diff_Object1= transfArray[0]
 diff_Object2= transfArray[1]
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-let table = createTable3(obj1, obj2, diff_Object1, diff_Object2);
-root.render(table);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// let table = createTable3(obj1, obj2, diff_Object1, diff_Object2);
+// root.render(table);
+const root1 = ReactDOM.createRoot(document.getElementById("root1"));
 
+// let table = createTable3(obj1, obj2, diff_Object1, diff_Object2);
+// root.render(table);
 
+//------------------------ Промисы --------------------------------------------------------------------------
+
+// const callBackend = (result) => new Promise((resolve, reject) => 
+// setTimeout(() =>
+//  resolve(result), какая-то задержка, например с помощью Math.random))
+
+function getRandomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function createRandomObj(length, currentDepth, depth){
+  let obj={};
+  let randomPrimitiveValueSelector;
+  let keyName;
+
+  if (currentDepth != 0) {obj.filter=false}
+
+  for (let i=0; i <length; i++){
+    keyName ='Name'+(currentDepth) +'-'+i
+    randomPrimitiveValueSelector=getRandomInRange(0, 10) >9 ? true : false
+    if (randomPrimitiveValueSelector) {
+      obj[keyName]='prmtvData'+Math.round(getRandomInRange(0, 10))
+    } else {
+      if (currentDepth !=depth) {obj[keyName]=createRandomObj(length, currentDepth+1, depth)
+      }else {
+        obj[keyName]='prmtvData'+Math.round(getRandomInRange(0, 10))
+      }
+    }
+  }
+  return obj;
+}
+
+function makeFilterOn(obj){
+  let objWithOnFilter={};
+
+    Object.entries(obj).forEach(([key, value]) => { 
+      const backEndAnswer = new Promise((resolve, reject) => {
+        let Answer;  
+        let AnswerDelay;
+        Answer = Math.round(getRandomInRange(0,10))>5 ? true : false
+        AnswerDelay = getRandomInRange(1000,10000)
+        setTimeout(() => {
+          // console.log('860 ) x ', Answer)  
+          resolve(Answer)
+        }, AnswerDelay );
+        // resolve(Answer);
+      });
+
+      backEndAnswer
+      .then(
+        result => {
+          value = result
+          return result 
+        },
+        error => {
+          alert("Rejected: " + error); 
+        }
+      )
+
+      if ((typeof(value) === 'object')&&(value!=null)) {
+        value = makeFilterOn(value)
+      }
+      if (key ==='filter') {
+        // value = await test()
+        let x = backEndAnswer
+        .then(
+          result => {
+          // console.log('885 ) x ', value)
+        //     value = result
+            return result }
+        );
+        // value = getRandomInRange(0, 15) >7 ? true : false
+        // console.log('885 ) x ', value)
+      }
+      objWithOnFilter[key] = value
+  });
+  return objWithOnFilter
+}
+
+function filterObject(obj){
+  let filteredShallowObject={}
+  let filterOn=false;
+    Object.entries(obj).forEach(([key, value]) => {
+      // if (key ==='filter') {console.log('883) key ', key,'value', value)        }
+      if ((key ==='filter') && (value === true)) {
+        filterOn=true
+      } else {
+        if ((typeof(value) !='object')||(value ===null)) {
+          filteredShallowObject[key] = value
+        } else {
+          filteredShallowObject[key] = filterObject(value)
+        }
+      }
+    });
+
+    if ( filterOn===true) {filteredShallowObject = 'цензура'}
+  return filteredShallowObject
+}
+
+function promiseFiltration(){
+  const backEndAnswer = new Promise((resolve, reject) => {
+    let Answer;  
+    let AnswerDelay;
+    Answer = Math.round(getRandomInRange(0,10))>5 ? true : false
+    AnswerDelay = getRandomInRange(100,500)
+    setTimeout(() => {
+      console.log('926 ) рандомный Answer в промисе. пока не используется ', Answer)  
+      resolve(Answer)
+    }, AnswerDelay );
+    // reject('error'); 
+  });
+
+  backEndAnswer
+  .then(
+    result=>{
+    console.log('943) результат предыдущего: ', result)
+    let settedFiltersObject=initialObject;
+    if (typeof(settedFiltersObject["Name0-0"])==='object') {
+      settedFiltersObject["Name0-0"].filter=true
+      console.log('947) 2й then. Установка фильтра в Name0-0')
+    }
+    console.log('949) установленный фильтр: ',settedFiltersObject)
+    return (settedFiltersObject)
+    },
+    error => {
+      alert("939) Rejected: " + error); 
+      return error 
+    }
+  )
+  .then(
+    result=>{
+    console.log('956)  результат предыдущего: ', result)
+    let settedFiltersObject=initialObject;
+    if (typeof(settedFiltersObject["Name0-1"])==='object') {
+      settedFiltersObject["Name0-1"].filter=true
+      console.log('961) 3й then. Установка фильтра в Name0-1')
+    }
+    console.log('962) установленный фильтр: ',settedFiltersObject)
+    return (settedFiltersObject)
+    }
+  )  
+  .then(
+    result=>{
+      console.log('968) результат предыдущего: ', result)
+      let filteredObject =filterObject(result)
+      return (console.log('969) отфильтрованный обьект', filteredObject))
+    }
+  )
+  .catch(function (err) {
+    console.log('973) ',err)
+  })
+  .finally(function () {
+    console.log('976 The end. finally')
+  })
+}
+
+let testAnswer = promiseFiltration()
+
+// Эти 6 строчек ниже ОК!
+let initialObject=createRandomObj(3, 0, 2)
+console.log('960) initialObject: ', initialObject)
+// let settedFiltersObject = makeFilterOn(initialObject)
+// console.log('915) ', settedFiltersObject)
+// let filteredObject =filterObject(settedFiltersObject)
+// console.log('917) ', filteredObject)
+
+// let filterNameArray=['f1', 'f2', 'f3', 'f4', 'f5']
+// let backEndAnswersArray=[]
+// let promiseArray=filterNameArray.push()
+
+// backEndAnswer
+//   .then(
+//     result => {
+//       result
+//       // backEndAnswersArray[0] = result
+//       // console.log(backEndAnswersArray[0])
+//     },
+//     error => {
+//       alert("Rejected: " + error); 
+//     }
+//   );
+
+  // backEndAnswersArray = filterNameArray.map(function (item) {
+  //   return (backEndAnswer)
+  //   backEndAnswersArray[item]={filterName:null, FilterYesOrNot:null, AnswerDelay:null }
+  //   backEndAnswersArray[item].filterName=item
+  //   backEndAnswersArray[item].FilterYesOrNot=""
+  //   backEndAnswersArray[item].AnswerDelay=""
+  // })
+  
+// for (let i=0; i<15; i++){
+//   console.log(Math.round(getRandomInRange(0,10))>5 ? true : false)
+  // console.log(backEndAnswer)
+// }
+// backEndAnswer1 = Math.round(getRandomInRange(0,1))===0 ? true : false
+// console.log(backEndAnswersArray)
